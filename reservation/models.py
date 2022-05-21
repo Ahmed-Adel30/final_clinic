88a911411e9dbfd6c069fcbe7717e5ef3595cdbe
Mozasbugs <ezz8120@gmail.com>
2022-05-21 17:26:49 +0200

    model comiit

--- reservation/models.py
+++ reservation/models.py
@@ -1,12 +1,41 @@
+from distutils.command import upload
 from django.db import models
 from django.contrib.auth.models import User
+from matplotlib import image
 # Create your models here.
 
-
-# class Doctor(models.Model):
+#class Specialty(models.Model):
+#    ID = models.CharField(max_length=50)
+    
+class Doctor(models.Model):
+    x= [
+        ('Dentistry','Dentistry'),
+        ('Psychiatry','Psychiatry'),
+        ('Neurology ','Neurology '),
+        ('Ear, Nose and Throat','Ear, Nose and Throat'),
+        ('Chest and Respiratory','Chest and Respiratory'),
+        ('Heart','Heart'),
+        ('Brain & Nerves','Brain & Nerves'),
+        ('Bones','Bones'),
+        (' Male Infertility',' Male Infertility'),
+        ('Pediatrics and New Born','Pediatrics and New Born'),
+    ]
+    
+    FirstName = models.CharField(max_length=50, verbose_name='First Name')
+    LastName = models.CharField(max_length=50, verbose_name='Last Name')
+    E_mail = models.CharField(max_length=50, verbose_name='E-Mail')
+    phonenum = models.DecimalField(max_length=50,verbose_name='Phone Number')
+    Specialty = models.CharField(max_length=50,verbose_name='Specialty', choices=x)     
+    location = models.CharField(max_length=50)
+    price = models.CharField(max_length=50,verbose_name='Fees', default='$')
+    image = models.ImageField(upload_to ='photos')
+    
+    
+    
+     
 #     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="v2")
 
-#     location = models.CharField(max_length=50)
 
 #     def is_admin(self):
 #         return self.user.is_superuser == 1
+
