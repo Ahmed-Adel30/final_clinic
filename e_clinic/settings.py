177b189d29ec31ecb5466371342177f6f17877d1
Ahmed-Adel30 <chocleate@gmail.com>
2022-05-21 21:26:24 +0200

    model commit
    created model for doctor

--- e_clinic/settings.py
+++ e_clinic/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'registration',
+    'reservation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +123,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     
 ]
+
+#MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- reservation/admin.py
+++ reservation/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Doctor
+from .models import Doctors_profile
 
 # Register your models here.
 
-admin.site.register(Doctor)
+admin.site.register(Doctors_profile)
