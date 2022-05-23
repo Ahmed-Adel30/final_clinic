e683cf3b6e3cfe3b451fe2cba36642333879e31f
Mozasbugs <ezz8120@gmail.com>
2022-05-23 13:56:07 +0200

    erroreorrorerrorerrorCommit

--- e_clinic/urls.py
+++ e_clinic/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('registration.urls')),
     path('app2/', include('reservation.urls')),
+   # path('app3/', include('payment.urls')),
 
 ]
 if settings.DEBUG:
