62cb49586db3366afbaea415bc2cc42f89061cc1
Mozasbugs <ezz8120@gmail.com>
2022-05-24 00:59:39 +0200

    ...

--- e_clinic/settings.py
+++ e_clinic/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'registration',
     'reservation',
-    'paycredit',
+   # 'paycredit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
