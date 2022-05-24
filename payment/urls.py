8e5492ab9c7a7ccede50b76e8d9b56cc08e1fd3d
Ahmed-Adel30 <chocleate@gmail.com>
2022-05-24 02:35:14 +0200

    Revert "commit test"
    This reverts commit 2746fd6a97e0be614efe6f5e54a0f129d11a16b5.

--- payment/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('credit',views.pay,name='credit'),
-]
--- payment/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def pay(request):
-    return render(request,"payment/credit_card.html")
