1766b2091fec4e57c308b3896eff1d2832d3ab69
Ahmed-Adel30 <chocleate@gmail.com>
2022-05-30 10:03:12 +0200

    commit
    better front-end
    logout function done

--- registration/urls.py
+++ registration/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',views.my_views,name='registration'),
     path('register',views.register,name='register'),
     path('login',views.log,name='log'),
-    path('LogOut',views.logout,name='LogOut'),
+    path('LogOut',views.logOutUser,name='LogOut'),
     path('LearnMore',views.lm,name='LearnMore'),
 ]
